49517308cb694a6a0bb9eb64624f3c72222ba2c7
S6654F <adomas.maciulaitis@seb.se>
2023-09-10 14:25:49 +0300

    MVP to consume messages

--- Worker.cs
+++ Worker.cs
@@ -1,20 +1,45 @@
+using Confluent.Kafka;
+
 namespace ProductivityTrackerService
 {
     public class Worker : BackgroundService
     {
         private readonly ILogger<Worker> _logger;
+        private readonly IConfiguration _consumerConfig;
 
-        public Worker(ILogger<Worker> logger)
+        public Worker(ILogger<Worker> logger, IConfiguration config)
         {
             _logger = logger;
+            _consumerConfig = config;
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
+            var topic = _consumerConfig.GetValue<string>("Topic");
+
+            var consumerConfig = new ConsumerConfig
+            {
+                GroupId = _consumerConfig.GetValue<string>("ConsumerName"),
+                BootstrapServers = _consumerConfig.GetValue<string>("Broker"),
+                AutoOffsetReset = AutoOffsetReset.Latest
+            };
+
+            using var consumer = new ConsumerBuilder<Null, string>(consumerConfig).Build();
+            consumer.Subscribe(topic);
+
+            consumer.Assign(new TopicPartitionOffset(topic, new Partition(0), Offset.Beginning));
+
             while (!stoppingToken.IsCancellationRequested)
             {
                 _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-                await Task.Delay(1000, stoppingToken);
+
+                var response = await Task.Run(() => consumer.Consume(stoppingToken), stoppingToken);
+
+                if (response.Message != null) 
+                {
+                    _logger.LogInformation($"Message consumed: {response.Message.Value}");
+                    //_logger.LogInformation($"Message consumed: {message?.Id} {message?.Number}");
+                }
             }
         }
     }
