91f237a338ab85943b38ac2b7480c104b5612ae6
S6654F <adomas.maciulaitis@seb.se>
2023-09-11 17:25:55 +0300

    Add db context

---
+++ Data/DayEntryEntity.cs
@@ -0,0 +1,16 @@
+﻿namespace ProductivityTrackerService.Data
+{
+    public class DayEntryEntity
+    {
+        public int Id { get; set; }
+        public DateTime AddedTimestamp { get; set; }
+        public DateTime Date { get; set; }
+        public string? WeekDay { get; set; }
+        public TimeSpan WakeUpTime { get; set; }
+        public TimeSpan ScreenTime { get; set; }
+        public TimeSpan ProjectWork { get; set; }
+        public bool WentToGym { get; set; }
+        public int Score { get; set; }
+        public string? Description { get; set; }
+    }
+}
