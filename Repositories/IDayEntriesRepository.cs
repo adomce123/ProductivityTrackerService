0a5a3e1233b18a8195cc70de51f88ad4f4dd55f9
S6654F <adomas.maciulaitis@seb.se>
2023-09-12 10:33:41 +0300

    day entries repository/service added

---
+++ Repositories/IDayEntriesRepository.cs
@@ -0,0 +1,10 @@
+﻿using ProductivityTrackerService.Data;
+
+namespace ProductivityTrackerService.Repositories
+{
+    public interface IDayEntriesRepository
+    {
+        Task<IEnumerable<DayEntryEntity>> GetDayEntriesAsync();
+        Task InsertDayEntriesAsync(IEnumerable<DayEntryEntity> dayEntries);
+    }
+}
---
+++ Services/IDayEntriesService.cs
@@ -0,0 +1,10 @@
+﻿using ProductivityTrackerService.Data;
+
+namespace ProductivityTrackerService.Services
+{
+    public interface IDayEntriesService
+    {
+        Task<IEnumerable<DayEntryEntity>> GetDayEntriesAsync();
+        Task InsertDayEntriesAsync(IEnumerable<DayEntryEntity> dayEntries);
+    }
+}
