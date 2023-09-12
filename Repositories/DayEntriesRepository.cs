7e0d8f52eb3a5a728a93e780dbc148a97f854ec5
S6654F <adomas.maciulaitis@seb.se>
2023-09-12 10:33:41 +0300

    day entries repository/service added

---
+++ Repositories/DayEntriesRepository.cs
@@ -0,0 +1,26 @@
+﻿using EFCore.BulkExtensions;
+using Microsoft.EntityFrameworkCore;
+using ProductivityTrackerService.Data;
+
+namespace ProductivityTrackerService.Repositories
+{
+    public class DayEntriesRepository : IDayEntriesRepository
+    {
+        private readonly ProductivityServiceDbContext _dbContext;
+
+        public DayEntriesRepository(ProductivityServiceDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<IEnumerable<DayEntryEntity>> GetDayEntriesAsync()
+        {
+            return await _dbContext.DayEntries.OrderBy(entry => entry.Id).ToListAsync();
+        }
+
+        public async Task InsertDayEntriesAsync(IEnumerable<DayEntryEntity> dayEntries)
+        {
+            await _dbContext.BulkInsertAsync(dayEntries);
+        }
+    }
+}
---
+++ Services/DayEntriesService.cs
@@ -0,0 +1,17 @@
+﻿using ProductivityTrackerService.Data;
+
+namespace ProductivityTrackerService.Services
+{
+    public class DayEntriesService : IDayEntriesService
+    {
+        public Task<IEnumerable<DayEntryEntity>> GetDayEntriesAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task InsertDayEntriesAsync(IEnumerable<DayEntryEntity> dayEntries)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
