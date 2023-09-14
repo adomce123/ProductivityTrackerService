5a92683a4e13b5c8305ff473bb520eb243007891
S6654F <adomas.maciulaitis@seb.se>
2023-09-14 11:31:41 +0300

    fixed scoped issue kafka msg -> insert to db is working

---
+++ Configuration/SerializerConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json;
+
+namespace ProductivityTrackerService.Configuration
+{
+    public static class SerializerConfiguration
+    {
+        public static JsonSerializerOptions DefaultSerializerOptions =>
+            new JsonSerializerOptions()
+            {
+                PropertyNameCaseInsensitive = true,
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+            };
+    }
+}
