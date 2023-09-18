babdc549d97dc997c24e78e8ea1de0ecb2240ad2
S6654F <adomas.maciulaitis@seb.se>
2023-09-18 17:37:59 +0300

    Unit tests for service

---
+++ ProductivityTrackerService.Tests/DayEntriesServiceShould.cs
@@ -0,0 +1,96 @@
+using AutoFixture;
+using FluentAssertions;
+using Moq;
+using ProductivityTrackerService.Data;
+using ProductivityTrackerService.Models;
+using ProductivityTrackerService.Models.Extensions;
+using ProductivityTrackerService.Repositories;
+using ProductivityTrackerService.Services;
+using Xunit;
+
+namespace ProductivityTrackerService.Tests
+{
+    public class DayEntriesServiceShould
+    {
+        private readonly Fixture _fixture;
+        private readonly Mock<IDayEntriesRepository> _dayEntriesRepositoryMock;
+        private readonly DayEntriesService _dayEntriesService;
+
+        public DayEntriesServiceShould()
+        {
+            _fixture = new Fixture();
+
+            _dayEntriesRepositoryMock = new Mock<IDayEntriesRepository>();
+
+            _dayEntriesService = new DayEntriesService(_dayEntriesRepositoryMock.Object);
+        }
+
+        [Fact]
+        public async Task ReturnDayEntries()
+        {
+            //ARRANGE
+            var dayEntryEntities = _fixture.Create<IEnumerable<DayEntryEntity>>();
+
+            _dayEntriesRepositoryMock
+                .Setup(_ => _.GetDayEntriesAsync())
+                .ReturnsAsync(dayEntryEntities);
+
+            //ACT
+            var result = await _dayEntriesService.GetDayEntriesAsync();
+
+            //ASSERT
+            result.Should().BeEquivalentTo(dayEntryEntities);
+
+            _dayEntriesRepositoryMock.VerifyAll();
+        }
+
+        [Fact]
+        public async Task InsertDayEntries()
+        {
+            //ARRANGE
+            var dayEntryDtos = _fixture.Create<IEnumerable<DayEntryDto>>();
+
+            _dayEntriesRepositoryMock
+                .Setup(_ => _.InsertDayEntriesAsync(It.IsAny<IEnumerable<DayEntryEntity>>()));
+
+            //ACT
+            await _dayEntriesService.InsertDayEntriesAsync(dayEntryDtos);
+
+            //ASSERT
+            _dayEntriesRepositoryMock.VerifyAll();
+        }
+
+        [Fact]
+        public void ConvertDtosToEntities()
+        {
+            //ARRANGE
+            var dayEntryDtos = _fixture.Create<IEnumerable<DayEntryDto>>();
+
+            var expectedEntities = new List<DayEntryEntity>();
+
+            foreach(var dayEntry in dayEntryDtos)
+            {
+                expectedEntities.Add(new DayEntryEntity
+                {
+                    Id = dayEntry.Id,
+                    AddedTimestamp = DateTime.UtcNow,
+                    Date = dayEntry.Date,
+                    WeekDay = dayEntry.WeekDay,
+                    WakeUpTime = dayEntry.WakeUpTime,
+                    ScreenTime = dayEntry.ScreenTime,
+                    ProjectWork = dayEntry.ProjectWork,
+                    WentToGym = dayEntry.WentToGym,
+                    Score = dayEntry.Score,
+                    Description = dayEntry.Description
+                });
+            }
+
+            //ACT
+            var actualEntities = dayEntryDtos.ToEntities();
+
+            //ASSERT
+            actualEntities.Should().BeEquivalentTo(expectedEntities, option => option
+                .Excluding(entity => entity.AddedTimestamp));
+        }
+    }
+}
