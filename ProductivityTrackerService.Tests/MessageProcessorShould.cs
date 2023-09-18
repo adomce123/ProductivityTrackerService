0fea3fbfb24a5fd3000370fbe0573837e393d2e4
S6654F <adomas.maciulaitis@seb.se>
2023-09-18 17:37:59 +0300

    Unit tests for service

---
+++ ProductivityTrackerService.Tests/MessageProcessorShould.cs
@@ -0,0 +1,49 @@
+﻿using AutoFixture;
+using Confluent.Kafka;
+using Moq;
+using ProductivityTrackerService.Data;
+using ProductivityTrackerService.Repositories;
+using ProductivityTrackerService.Services;
+using Xunit;
+
+namespace ProductivityTrackerService.Tests
+{
+    public class MessageProcessorShould
+    {
+        private readonly Fixture _fixture;
+        private readonly Mock<IDayEntriesRepository> _dayEntriesRepositoryMock;
+        private readonly Mock<IDayEntriesService> _dayEntriesServiceMock;
+        private readonly MessageProcessor _messageProcessor;
+
+        public MessageProcessorShould()
+        {
+            _fixture = new Fixture();
+
+            _dayEntriesRepositoryMock = new Mock<IDayEntriesRepository>();
+
+            _dayEntriesServiceMock = new Mock<IDayEntriesService>();
+
+            _messageProcessor = new MessageProcessor(_dayEntriesServiceMock.Object);
+        }
+
+        [Fact]
+        public async Task NotDoAnythingIfPartitionEofAndMessageListIsEmpty()
+        {
+            //ARRANGE
+            var message = new ConsumeResult<Null, string>()
+            {
+                IsPartitionEOF = true
+            };
+
+
+            var dayEntryEntities = _fixture.Create<IEnumerable<DayEntryEntity>>();
+
+            await _messageProcessor.ProcessAsync(message);
+
+            _dayEntriesRepositoryMock
+                .Verify(repository => repository
+                .InsertDayEntriesAsync(dayEntryEntities), Times.Never());
+        }
+
+    }
+}
