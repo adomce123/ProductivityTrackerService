c696edc2023f7640882fd47f85ce89a7d01fc50e
S6654F <adomas.maciulaitis@seb.se>
2023-10-03 11:20:22 +0300

    Add JsonRequired for validate object before deserializing

--- ProductivityTrackerService/MessageConsumer.cs
+++ ProductivityTrackerService/MessageConsumer.cs
@@ -6,40 +6,52 @@ namespace ProductivityTrackerService
     public class MessageConsumer : BackgroundService
     {
         private readonly ILogger<MessageConsumer> _logger;
-        private readonly IConfiguration _configuration;
+        private readonly ConsumerConfiguration _consumerConfiguration;
         private readonly IMessageProcessor _messageProcessor;
+        private readonly IConsumer<Null, string> _consumer;
 
         public MessageConsumer(
             ILogger<MessageConsumer> logger, 
-            IConfiguration consumerConfiguration,
+            IConfiguration configuration,
             IMessageProcessor messageProcessor)
         {
             _logger = logger;
-            _configuration = consumerConfiguration;
             _messageProcessor = messageProcessor;
+
+            _consumerConfiguration = configuration.GetSection("DayEntriesConsumer")
+                .Get<ConsumerConfiguration>()
+                ?? throw new ArgumentException("Were not able to Consumer configuration");
+
+            _consumer = new ConsumerBuilder<Null, string>(_consumerConfiguration.ConsumerConfig)
+                .Build();
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            var consumerConfiguration = _configuration.GetSection("DayEntriesConsumer")
-                .Get<ConsumerConfiguration>()
-                ?? throw new ArgumentException("Were not able to Consumer configuration");
-
-            using var consumer =
-                new ConsumerBuilder<Null, string>(consumerConfiguration.ConsumerConfig).Build();
-
-            consumer.Subscribe(consumerConfiguration.Topic);
+            _consumer.Subscribe(_consumerConfiguration.Topic);
 
             try
             {
                 while (!stoppingToken.IsCancellationRequested)
                 {
-                    var response = await Task.Run(() => consumer
+                    var response = await Task.Run(() => _consumer
                         .Consume(stoppingToken), stoppingToken);
 
-                    _logger.LogInformation($"Message consumed: {response.Message}");
+                    _logger.LogInformation("Message consumed: {response.Message}", response.Message);
 
-                    await _messageProcessor.ProcessAsync(response);
+                    try
+                    {
+                        await _messageProcessor.ProcessAsync(response);
+                    }
+                    catch (Exception ex)
+                    {
+                        _logger.LogInformation("Message processing failed with exception {ex}", ex.Message);
+                    }
+                    finally
+                    {
+                        if (!response.IsPartitionEOF)
+                            StoreOffset(response);
+                    }
                 }
             }
             catch (OperationCanceledException) 
@@ -48,8 +60,17 @@ namespace ProductivityTrackerService
             }
             catch (Exception ex) 
             {
-                _logger.LogCritical(ex, "A critical exception was thrown.");
+                _logger.LogCritical(ex.Message, "A critical exception was thrown. Discarding message");
             }
+            finally
+            {
+                _consumer.Dispose();
+            }
+        }
+
+        private void StoreOffset(ConsumeResult<Null, string> message)
+        {
+            _consumer.StoreOffset(message);
         }
     }
 }
